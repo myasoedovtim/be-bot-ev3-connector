a73cdf6340c263984e5f2d5fe4a335e054a6dd16
tim <myasoedov.tim@gmail.com>
2024-12-16 01:36:56 +0300

    proccessing incomming messages

--- be_bot_ev3_connector/main.py
+++ be_bot_ev3_connector/main.py
@@ -7,7 +7,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from umqtt.robust import MQTTClient
 import time
-
+import json
 #from pybricks.media.ev3dev import SoundFile, ImageFile
 
 ev3 = EV3Brick()
@@ -27,40 +27,41 @@ message = {
   "description": "My first robot with bebot",
   "type": "test",
   "is_active": True,
-  "actions": [],
-  "sensors": []
+  "actions": ["forward","backward"],
+  "sensors": ["Infrared"]
 }
 def getmessages(topic, msg):
         ev3.screen.print(str(msg.decode()))
+        data = json.loads(msg.decode())
+        ev3.screen.print(data)
+        if "forward" in data:
+             robot.straight(-data["forward"])
+        if "backward" in data:
+             robot.straight(data["backward"])
+        if "getsensor" in data:
+             if data["getsensor"] == "Infrared":
+                  ev3.screen.print(obstacle_sensor.distance())
+                  sensor_message = {"device_id": MQTT_ClientID,"name": "Infrared","value": obstacle_sensor.distance()}
+                  client.publish("/bebot/to/api/sensor", str(sensor_message))
+
 
 client.publish("/bebot/to/api/init", str(message))
 client.set_callback(getmessages)
 client.subscribe("/bebot/device/"+MQTT_ClientID)
 
 obstacle_sensor = InfraredSensor(Port.S4)
-ev3.speaker.beep()
-
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
-
-
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=160)
-robot.straight(50)
 
+ev3.speaker.beep()
+
+counter = 0
 while True:
+    counter+=1
     client.check_msg()
     time.sleep(0.1)
-
-'''
-while True:
-    
-    while obstacle_sensor.distance() > 20:
-        ev3.screen.print(obstacle_sensor.distance())
-        robot.drive(-200, 0)
-        wait(5)
-    
-    robot.straight(50)
-    robot.turn(700)
-    wait(5)
-'''
+    if counter%1000 == 0:
+        ev3.screen.print("Time to send status...")
+        client.publish("/bebot/to/api/status", str(message))
 
