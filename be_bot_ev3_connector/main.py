a2f67df5964659a7110a846e387c3f962306d3bf
tim <myasoedov.tim@gmail.com>
2024-12-15 20:33:59 +0300

    refresh gitignore, add requirements

--- be_bot_ev3_connector/main.py
+++ be_bot_ev3_connector/main.py
@@ -8,9 +8,6 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
 
 # Create your objects here.
 ev3 = EV3Brick()
