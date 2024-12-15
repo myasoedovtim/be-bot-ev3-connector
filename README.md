19ac5986a3fd6e3f490fca83e4323dfcaf47711a
tim <myasoedov.tim@gmail.com>
2024-12-15 23:52:49 +0300

    add mqtt

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # EV3 connector
 Программа коннектор для усройства Lego EV3. Реализует подключение к системе be-bot.online.
+
+## Virtual Enviroment
+
+### Create new enviroment
+```bash
+ python -m venv venv
+```
+
+### Activate on windows
+```bash
+ venv\Scripts\activate.bat
+```
+### Activate on linux
+```bash
+ source venv/bin/activate
+```
+
+## Install libs
+
+```bash
+ pip install -r requirements.txt
+```
--- be_bot_ev3_connector/main.py
+++ be_bot_ev3_connector/main.py
@@ -5,15 +5,25 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from umqtt.robust import MQTTClient
+
+#from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+MQTT_ClientID = "1fa419a0-a9fa-4866-a1e2-a0e651ec0b29"
+MQTT_HOST="m6.wqtt.ru"
+MQTT_PORT=17810
+MQTT_USER=""
+MQTT_PASSWORD=""
 
-# Create your objects here.
+client = MQTTClient(MQTT_ClientID, MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASSWORD)
+client.connect()
+
+client.publish("/robot/topic", MQTT_ClientID + ' Started')
+
 ev3 = EV3Brick()
 
 obstacle_sensor = InfraredSensor(Port.S4)
-# Write your program here.
 ev3.speaker.beep()
 
 left_motor = Motor(Port.A)
@@ -21,8 +31,9 @@ right_motor = Motor(Port.D)
 
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=160)
+robot.straight(50)
 
-
+'''
 while True:
     
     while obstacle_sensor.distance() > 20:
@@ -33,4 +44,5 @@ while True:
     robot.straight(50)
     robot.turn(700)
     wait(5)
+'''
 
