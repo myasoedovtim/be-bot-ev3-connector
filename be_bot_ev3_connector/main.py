b1f64ed21bc80498cd74dd6773e6c00978a4ea1d
tim <myasoedov.tim@gmail.com>
2024-12-16 00:29:48 +0300

    add getmessages

--- be_bot_ev3_connector/main.py
+++ be_bot_ev3_connector/main.py
@@ -6,9 +6,11 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from umqtt.robust import MQTTClient
+import time
 
 #from pybricks.media.ev3dev import SoundFile, ImageFile
 
+ev3 = EV3Brick()
 
 MQTT_ClientID = "1fa419a0-a9fa-4866-a1e2-a0e651ec0b29"
 MQTT_HOST="m6.wqtt.ru"
@@ -28,9 +30,12 @@ message = {
   "actions": [],
   "sensors": []
 }
-client.publish("/bebot/to/api/init", str(message))
+def getmessages(topic, msg):
+        ev3.screen.print(str(msg.decode()))
 
-ev3 = EV3Brick()
+client.publish("/bebot/to/api/init", str(message))
+client.set_callback(getmessages)
+client.subscribe("/bebot/device/"+MQTT_ClientID)
 
 obstacle_sensor = InfraredSensor(Port.S4)
 ev3.speaker.beep()
@@ -41,6 +46,10 @@ right_motor = Motor(Port.D)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=160)
 robot.straight(50)
+
+while True:
+    client.check_msg()
+    time.sleep(0.1)
 
 '''
 while True:
